bfd47b362808d99759bfd9787e0a55b5e6890ecf
MichailBolsun32 <bolsun32@mail.ru>
2024-09-07 14:14:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+number_tasks = 12
+number_hours = 1.5
+course_name = "Python"
+time_task = number_hours / number_tasks
+print('Курс: ', course_name, ', всего задач:', number_tasks, ', затрачено часов:', number_hours,
+      ', среднее время выполнения ', time_task, ' часа.', sep='')
